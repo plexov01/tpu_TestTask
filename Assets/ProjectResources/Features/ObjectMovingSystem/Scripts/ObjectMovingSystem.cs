4deb94e7b10097e21b14c37cde25116125b32d48
plexov01 <plexov.01@mail.ru>
2024-03-14 18:40:10 +0700

    При нажатии на объект он берётся в руку

---
+++ Assets/ProjectResources/Features/ObjectMovingSystem/Scripts/ObjectMovingSystem.cs
@@ -0,0 +1,70 @@
+namespace TPU_TestTask.Features.ObjectMovingSytem
+{
+    using Camera;
+    using Object;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEngine;
+
+    /// <summary>
+    /// Система, перемещающая объекты
+    /// </summary>
+    public class ObjectMovingSystem : MonoBehaviour
+    {
+
+        private List<ResearchObject> _researchObjects = new List<ResearchObject>();
+        private MainCamera _mainCamera = default;
+
+        private Vector3 _pointScreen = default;
+        private GameObject _movingObject = default;
+        private void Start()
+        {
+            _mainCamera = FindObjectOfType<MainCamera>();
+            
+            _researchObjects = FindObjectsOfType<ResearchObject>().ToList();
+            
+            foreach (var researchObject in _researchObjects)
+            {
+                researchObject.onObjectClicked += MoveObject;
+            }
+        }
+        /// <summary>
+        /// Передвинуть объект
+        /// </summary>
+        /// <param name="movingObject"></param>
+        public void MoveObject(GameObject movingObject)
+        {
+            _movingObject = movingObject;
+            
+
+            _pointScreen = _mainCamera.Camera.WorldToScreenPoint(_movingObject.transform.position);
+            _pointScreen.z = 2f;
+
+            _movingObject.transform.rotation = _mainCamera.transform.rotation;
+
+        }
+
+        private void FixedUpdate()
+        {
+            if (_movingObject != null)
+            {
+                //Двигаем объект за курсором
+                Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _pointScreen.z);
+                Vector3 curPosition = _mainCamera.Camera.ScreenToWorldPoint(curScreenPoint);
+                _movingObject.transform.position = Vector3.Lerp(_movingObject.transform.position,curPosition , 5f * Time.deltaTime);
+                
+                Debug.Log(curPosition);
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (var researchObject in _researchObjects)
+            {
+                researchObject.onObjectClicked -= MoveObject;
+            }
+        }
+        
+    }
+
+}
--- Assets/ProjectResources/Features/Objects/Scripts/ResearchObject.cs
+++ Assets/ProjectResources/Features/Objects/Scripts/ResearchObject.cs
@@ -1,14 +1,24 @@
 namespace  TPU_TestTask.Features.Object
 {
+    using Camera;
+    using System;
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
     /// <summary>
     /// Объект, который совмещается с инструментом
     /// </summary>
-    public class Object : MonoBehaviour
+    public class ResearchObject : MonoBehaviour
     {
+        public event Action<GameObject> onObjectClicked = delegate {  };
         [SerializeField] private List<ObjectState> _objectStates = new List<ObjectState>();
+        
+
+        private void OnMouseDown()
+        {
+            
+            onObjectClicked(gameObject);
+        }
     }
 
 }
